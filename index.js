e0410a0bbb2de28f246cc3c9fb06d467defb7fde
zahard <obedgarcia1710@gmail.com>
2023-09-10 20:21:22 -0600

    add video 00

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get("/", (req, res, nest) => {
     res.send("Bienvenido");
 });
 
+app.get("/:name", (req, res, nest) => {
+    res.status(200);
+    res.send("Hola,"+req.params.name);
+});
+
 app.listen(3000, ()=> {
     console.log("Server is runing...");
 });
