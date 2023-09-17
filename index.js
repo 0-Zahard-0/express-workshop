54d5e261d9d5da95e42b9c099a29e74d63f96017
0-Zahard-0 <Obedgarcia1710@gmail.com>
2023-09-17 17:24:49 -0600

    Peticiones por POST, operador ternario, método filter y body parser

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 const express = require('express');
 const app = express ();
 const { pokemon } = require('./pokedex.json');
-
-app.get("/", (req, res, nest) => {
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res, next) => {
+    return res.status(200).send("Bienvenido al Pokedex");
 });
-
-app.get('/pokemon/all', (req, res, nest) => {
-    res.status(200);
-    res.send(pokemon);
+app.post('/pokemon', (req, res, next) => {
+    return res.status(200).send(req.body);
 });
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => { 
-    
+app.get('/pokemon', (req, res, next) => {
+    return res.status(200).send(pokemon);
+});
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
     const id = req.params.id - 1;
-    if (id > 0 && id <= 150) {
-    res.status(200);
-    res.send(pokemon [req.params.id -1]);
-    }
-    
-    else res.status(484);
-    
-    res.send("Pokémon no encontrado");
-    
+    (id > 0 && id <= 150) ?
+        res.status(200).send(pokemon [req.params.id -1]) :
+        res.status(484).send("Pokémon no encontrado");
+
+});
+
+app.get('/pokemon/:name([A-Za-z]+)', (req,res, next) =>{
+    const name = req.params.name;
+    const pk = pokemon.filter((p)=>{
+        return (p.name.toUpperCase()===name.toUpperCase()) ? p : null;
+
     });
-    
-    app.get('/pokemon/:name', (req, res, next) =>{
-    const name = req.params.name; 
-    for (i = 0; i < pokemon.length; i++) { 
-        if (pokemon[i].name == name) {
-            res.status(200); 
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("Pokémon no encontrado");
-    
-    });
+    (pk.length > 0) ?
+        res.status(200).send(pk) :
+        res.status(404).send("pokemon no encontrado");
+});
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log("Server is runing...");
