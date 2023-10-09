720f8b0f90ea64ad0f9a28d0c4202ae2e20e90e5
0-Zahard-0 <Obedgarcia1710@gmail.com>
2023-10-08 19:48:58 -0600

    Instalando JWT

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const user = express.Router();
 const db = require('../config/database');
+const jwt = require("jsonwebtoken");
 const {query} = require("express");
 
 user.post('/', async (req, res, next) => {
@@ -14,6 +15,24 @@ user.post('/', async (req, res, next) => {
             return res.status(201).json({code: 201, message: "Usuario registrado correctamente"});
         }
         return res.status(500).json({code: 500, message: "Ocurrió un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+user.post("/login", async (req, res, next) => {
+    const {user_mail, user_password} = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+    const rows = await db.query(query);
+
+    if (user_mail && user_password) {
+        if (rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }   , "debugkey");
+            return res.status(200).json({code: 200, message: token});
+        }else {
+            return res.status(200).json({code: 401, message: "Usuario y/o contraseña incorrectos"});
+        }
     }
     return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
